c156395fc85f9e65f5415312d2c04ed45b66bb9c
Brandon Hung <brandonhung343@gmail.com>
2019-11-21 20:36:30 -0500

    Backend basic querying and other db functions

---
+++ libdb.py
@@ -0,0 +1,105 @@
+import sqlite3
+from sqlite3 import Error
+
+# columns is a dictionary with the key being the name, value being the type
+def new_table(db_name, table_name, columns):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    col_string = ''
+    for key in columns:
+    	col_string = col_string + str(key) + ' ' + str(columns[key] + ', ')
+    col_string = col_string.strip(', ')
+    c.execute('CREATE TABLE {tn} ({cs})'.format(tn=table_name, cs=col_string))
+    print('DB created')
+    conn.commit()
+    conn.close()
+
+
+# each field is a string
+def add_col(db_name, table_name, col_name, col_type, def_value=None):
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	if def_value is not None:
+	    c.execute('ALTER TABLE {tn} ADD COLUMN {cn} {ct} DEFAULT {df}'.format(tn=table_name, cn=col_name, ct=col_type, df=def_value))
+	else:
+	    c.execute('ALTER TABLE {tn} ADD COLUMN {cn} {ct}'.format(tn=table_name, cn=col_name, ct=col_type))
+	conn.commit()
+	conn.close()
+
+# columns and values are tuples of data in the right places
+# columns is going to be all strings, while values is 
+# the value of the correct data type in the matching space
+# ex. insert_entry('testdb', 'persons', ('name', 'id', 'machine', 'mill'), ('Marianne', '22211902', 1, 1))
+def insert_entry(db_name, table_name, columns, values):
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	col_string = ''
+	val_string = ''
+	for col in columns:
+		col_string = col_string + col + ', '
+	col_string = col_string.strip(', ')
+	for val in values:
+		val_string = val_string + '?, '
+	val_string = val_string.strip(', ')
+	sql_cmd = 'INSERT INTO {tn} ({cs}) VALUES({vs})'.format(tn=table_name, cs=col_string, vs=val_string)
+	c.execute(sql_cmd, values)
+	lastid = c.lastrowid
+	conn.commit()
+	conn.close()
+	return lastid
+ 
+def update_entry(db_name, table_name, entry_name, entry_value, columns, values):
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	col_string = ''
+	val_string = ''
+	for col in columns:
+		col_string = col_string + col + ' = ?, '
+	col_string = col_string.strip(', ')
+	# combine entry_value with values in tuple to make executing easier
+	cmd_tuple = list(values)
+	cmd_tuple.append(entry_value)
+	cmd_tuple = tuple(cmd_tuple)
+	sql_cmd = 'UPDATE {tn} SET {cs} WHERE {en} = ?'.format(tn=table_name, cs=col_string, en=entry_name)
+	c.execute(sql_cmd, cmd_tuple)
+	conn.commit()
+	conn.close()
+
+# column is a string, value is the serach condition (value and datatype) of the column 
+# value is a tuple (item,)
+def remove_entry(db_name, table_name, column, value):
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	# combine entry_value with values in tuple to make executing easier
+	sql_cmd = 'DELETE FROM {tn} WHERE {cs} = ?'.format(tn=table_name, cs=column)
+	c.execute(sql_cmd, value)
+	conn.commit()
+	conn.close()
+
+# query information about a single thing. 
+# supply a tuple to columns to limit the number of solumns returned, else returns all information about them
+def query_db(db_name, table_name, cond, values, columns=None):
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	col_string = ''
+	if columns is not None:
+		for col in columns:
+			col_string = col_string + col + ', '
+		col_string = col_string.strip(', ')
+		sql_cmd = 'SELECT {cs} FROM {tn} WHERE {cl} = ?'.format(cs=col_string, tn=table_name, cl=cond)
+	else:
+		sql_cmd = 'SELECT * FROM {tn} WHERE {cl} = ?'.format(tn=table_name, cl=cond)
+	c.execute(sql_cmd, values)
+	queried = c.fetchall()
+	conn.close()
+	return queried
+
+# print everyone in the table
+def get_table_info(db_name, table_name):
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	c.execute('SELECT * FROM {tn}'.format(tn=table_name))
+	info = c.fetchall()
+	for row in info:
+		print(row)
+	conn.close()
--- tooltron.py
+++ tooltron.py
@@ -1,121 +1,144 @@
-import sqlite3
+import libdb as db
 import time
-import RPi.GPIO as GPIO
-import MFRC522
-import signal
+# import RPi.GPIO as GPIO
+# # import MFRC522
+# import signal
 
-from sqlite3 import Error
+db_name = 'testdb'
+tb_name = 'persons'
+id_col = 'id_num'
+mill_col = 'mill'
+lathe_col = 'lathe'
+machine_col = 'machine'
 
-def db_new(name, *args, **kwargs):
-	if kwargs is not None:
-		counter = 0
-		conn = sqlite3.connect(name)
-		c = conn.cursor()
-		for key, value in kwargs.iteritems():
-			counter += 1
-			try:
-				c.execute("CREATE TABLE {tn} ({nf} {ft})").format(tn=args[counter], nf=key, ft=value)
-			except Error as e:
-				print(e)
-		print("DB created")
-		conn.commit()
-		conn.close()
-	else:
-		print("fields and types not properly created")
+# TODO: may need a validity column to see if they've expired in the last 4 years? or 5 years
+# TODO: possibly need to write an error correction code for sending stuff on the wifi
 
-def check_clearances(id, c, column_name, table_name, id_column):
-	c.execute("SELECT ({cn}) FROM {tn} WHERE {idf}={id}".format(cn=column_name, tn=table_name, idf=id_column, id=id))
-	all_rows = c.fetchone()
-	print(all_rows)
-	if (all_rows is not None and all_rows[0] != 0):
-		return True
-	return False
+# gets certifications by ID num, id is a string
+def mill_clearance(id_number):
+    query = db.query_db(db_name, tb_name, id_col, (id_number,), (mill_col,))
+    if len(query) > 0:
+        return query[0][0]
+    print("ID is not in the system")
+    
+def lathe_clearance(id_number):
+    return db.query_db(db_name, tb_name, id_col, (id_number,), (lathe_col,))
+    if len(query) > 0:
+        return query[0][0]
+    print("ID is not in the system")
+
+def machine_clearance(id_number):
+    return db.query_db(db_name, tb_name, id_col, (id_number,), (machine_col,))
+    if len(query) > 0:
+        return query[0][0]
+    print("ID is not in the system")
+
+# set the person's clearance bit
+def update_mill(id_number, cleared_or_not):
+    db.update_entry(db_name, tb_name, id_col, id_number, (mill_col,), (cleared_or_not,))
+
+def update_lathe(id_number, cleared_or_not):
+    db.update_entry(db_name, tb_name, id_col, id_number, (lathe_col,), (cleared_or_not,))
+
+def update_machine(id_number, cleared_or_not):
+    db.update_entry(db_name, tb_name, id_col, id_number, (machine_col,), (cleared_or_not,))
+
+# clearances is a list of 0 or 1s for machine, mill, and lathe
+def add_person(id_number, name, clearances):
+    # db columns
+    cols = ('name', id_col, machine_col, mill_col, lathe_col)
+    val_list = tuple([name, id_number] + clearances)
+    db.insert_entry(db_name, tb_name, cols, val_list)
+
+def remove_person(id_number):
+    db.remove_entry(db_name, tb_name, id_col, (id_number,))
+
+def view_db():
+    db.get_table_info(db_name, tb_name)
+
+# def power_on(relay):
+#     # assuming it latches
+#     GPIO.output(relay, GPIO.HIGH)
 
 
-def power_on(relay):
-    # assuming it latches
-    GPIO.output(relay, GPIO.HIGH)
+# def power_off(relay):
+#     GPIO.output(relay, GPIO.LOW)
 
+# # Capture SIGINT for cleanup when the script is aborted
+# def end_read(signal, frame):
+#     global continue_reading
+#     print
+#     "Ctrl+C captured, ending read."
+#     continue_reading = False
+#     GPIO.cleanup()
 
-def power_off(relay):
-    GPIO.output(relay, GPIO.LOW)
+# def main():
+#     print("Starting up")
+#     db = "" #insert name here
+#     conn = sqlite3.connect(db)
+#     c = conn.cursor()
+#     relay = 0 #pick a pin, any pin
+#     GPIO.setup(relay, GPIO.output)
+#     column_list = c.execute("PRAGMA table_info{db}".format(db = db)).fetchall()
+#     table_name = str(c.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchone())
+#     # adjust as necessary
+#     column_name = column_list[0]
+#     id_column = column_list[1]
 
+#     # Hook the SIGINT
+#     signal.signal(signal.SIGINT, end_read)
 
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal, frame):
-    global continue_reading
-    print
-    "Ctrl+C captured, ending read."
-    continue_reading = False
-    GPIO.cleanup()
+#     # Create an object of the class MFRC522
+#     MIFAREReader = MFRC522.MFRC522()
 
-def main():
-    print("Starting up")
-    db = "" #insert name here
-    conn = sqlite3.connect(db)
-    c = conn.cursor()
-    relay = 0 #pick a pin, any pin
-    GPIO.setup(relay, GPIO.output)
-    column_list = c.execute("PRAGMA table_info{db}".format(db = db)).fetchall()
-    table_name = str(c.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchone())
-    # adjust as necessary
-    column_name = column_list[0]
-    id_column = column_list[1]
+#     # Gotta figure out what to do about this loop
+#     # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+#     print("Start")
+#     debounce = 0
+#     while continue_reading:
 
-    # Hook the SIGINT
-    signal.signal(signal.SIGINT, end_read)
+#         # Scan for cards
+#         (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # Create an object of the class MFRC522
-    MIFAREReader = MFRC522.MFRC522()
+#         # Get the UID of the card
+#         (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
-    # Gotta figure out what to do about this loop
-    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
-    print("Start")
-    debounce = 0
-    while continue_reading:
+#         # If we have the UID, continue
+#         if status == MIFAREReader.MI_OK and debounce == 0:
+#             id_num = 0
+#             debounce = 5
+#             # Print UID
+#             print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+#             for i in range(4):
+#                 id_num += ((int(uid[i])) *(10 ** (3 * (3 - i))))
 
-        # Scan for cards
-        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+#             if check_clearances(id_num, c, column_name, table_name, id_column):
+#                 state = not state
+#                 power_on(relay) if state == 1 else power_off(relay)
 
-        # Get the UID of the card
-        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+#         elif debounce != 0:
+#             assert(debounce > 0)
+#             debounce -= 1
 
-        # If we have the UID, continue
-        if status == MIFAREReader.MI_OK and debounce == 0:
-            id_num = 0
-            debounce = 5
-            # Print UID
-            print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
-            for i in range(4):
-                id_num += ((int(uid[i])) *(10 ** (3 * (3 - i))))
+#         else:
+#             continue
 
-            if check_clearances(id_num, c, column_name, table_name, id_column):
-                state = not state
-                power_on(relay) if state == 1 else power_off(relay)
+#                 # Came with the thing, looks unnecessary
+#                 # # This is the default key for authentication
+#                 # key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
 
-        elif debounce != 0:
-            assert(debounce > 0)
-            debounce -= 1
+#                 # # Select the scanned tag
+#                 # MIFAREReader.MFRC522_SelectTag(uid)
 
-        else:
-            continue
+#                 # # Authenticate
+#                 # status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
 
-                # Came with the thing, looks unnecessary
-                # # This is the default key for authentication
-                # key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-
-                # # Select the scanned tag
-                # MIFAREReader.MFRC522_SelectTag(uid)
-
-                # # Authenticate
-                # status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
-                # # Check if authenticated
-                # if status == MIFAREReader.MI_OK:
-                #     MIFAREReader.MFRC522_Read(8)
-                #     MIFAREReader.MFRC522_StopCrypto1()
-                # else:
-                #     print "Authentication error"
+#                 # # Check if authenticated
+#                 # if status == MIFAREReader.MI_OK:
+#                 #     MIFAREReader.MFRC522_Read(8)
+#                 #     MIFAREReader.MFRC522_StopCrypto1()
+#                 # else:
+#                 #     print "Authentication error"
 
 
 
